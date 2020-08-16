322a91bf9f5b1bb4d8b3aa9b99b6ac1e0ceefa58
FedericoTartarini <federicotartarini@gmail.com>
2020-08-16 15:12:43 +0800

    added code to play the video

--- src/Components/MyMap.js
+++ src/Components/MyMap.js
@@ -1,18 +1,33 @@
-import React from "react";
-import { Map, TileLayer } from "react-leaflet";
+import React, { useState } from "react";
+import { Map, TileLayer, VideoOverlay } from "react-leaflet";
 
 function MyMap() {
-  const position = [1.35, 103.8];
+  const position = [25, -100];
+  const [play, setPlay] = useState(true);
+
+  function onTogglePlay() {
+    setPlay(!play);
+  }
+
   return (
     <Map
       className="map"
       center={position}
-      zoom={10}
-      style={{ height: 500, width: "100%" }}
+      zoom={4}
+      style={{ height: 800, width: "100%" }}
+      onClick={onTogglePlay}
     >
       <TileLayer
         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <VideoOverlay
+        bounds={[
+          [32, -130],
+          [13, -100],
+        ]}
+        play={play}
+        url="https://www.mapbox.com/bites/00188/patricia_nasa.webm"
       />
     </Map>
   );
