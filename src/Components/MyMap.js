8b032f133967cbdf3d8fd5acbac8cc6ffd1c9ccb
FedericoTartarini <federicotartarini@gmail.com>
2020-08-23 22:48:37 +0800

    added kml file

--- src/Components/MyMap.js
+++ src/Components/MyMap.js
@@ -1,8 +1,28 @@
 import React from "react";
 import { Map, TileLayer } from "react-leaflet";
+import ReactLeafletKml from "react-leaflet-kml";
+
+const kmlText = "";
+const parser = new DOMParser();
+const kml = parser.parseFromString(kmlText, "text/xml");
 
 function MyMap() {
   const position = [1.35, 103.8];
+
+  const [kml, setKml] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/aviklai/react-leaflet-kml/master/src/assets/example1.kml"
+    )
+      .then((res) => res.text())
+      .then((kmlText) => {
+        const parser = new DOMParser();
+        const kml = parser.parseFromString(kmlText, "text/xml");
+        setKml(kml);
+      });
+  }, []);
+
   return (
     <Map
       className="map"
@@ -14,6 +34,7 @@ function MyMap() {
         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <ReactLeafletKml kml={kml} />
     </Map>
   );
 }
